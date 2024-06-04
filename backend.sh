2c1a258d51aa008b124c551fafd249a0050cdc63
guru-sulakhe <sulakheguruprasad@gmail.com>
2024-06-04 16:38:11 +0530

    installing mysql

---
+++ backend.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+USERID=$(id -u) #script execute and store the output in USERID
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPTNAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log
+R="\e[31m" #for red color
+G="\e[32m" #for green color
+Y="\e[33m"
+N="\e[0m" #for normal color
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2.. $R FAILED $N"
+    else
+        echo -e "$2.. $G SUCCESSFUL $N" 
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "You dont have accees,only root user have access to install"
+else
+    echo "You are a super user"
+fi
+
+dnf module disable nodejs -y &>>$LOGFILE
+VALIDATE $? "Disabling nodejs"
+
+dnf module enable nodejs:20 -y &>>$LOGFILE
+VALIDATE $? "Enablinng nodjes version 20"
+
+dnf install nodejs -y &>>$LOGFILE
+VALIDATE $? "Installing nodejs"
+
