7eb406790765c77724fb7ec5703c0d59707752f3
guru-sulakhe <sulakheguruprasad@gmail.com>
2024-06-05 15:01:45 +0530

    backend service

---
+++ frontend.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+
+USERID=$(id -u) #script execute and store the output in USERID
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPTNAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log
+R="\e[31m" #for red color
+G="\e[32m" #for green color
+Y="\e[33m"
+N="\e[0m" #for normal color
+echo "Please enter db password: "
+read -s mysql_root_password
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2.. $R FAILED $N"
+    else
+        echo -e "$2.. $G SUCCESSFUL $N" 
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "You dont have accees,only root user have access to install"
+else
+    echo "You are a super user"
+fi
+
+dnf install nginx -y &>>$LOGFILE
+VALIDATE $? "installing nginx"
+
+systemctl enable nginx &>>$LOGFILE
+VALIDATE $? "Enabling nginx"
+
+systemctl start nginx &>>$LOGFILE
+VALIDATE $? "Starting nginx"
+
+rm -rf /usr/share/nginx/html/* 
+VALIDATE $? "Removing the default content"
+
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip
+VALIDATE $? "Downloading the frontend content in tmp directory"
+
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
+VALIDATE $? "unzipping the fronend content in usr directory"
+
